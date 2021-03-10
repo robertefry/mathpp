d9d0c67b7f1341b5e7e3c6a38a47b0cadfddbbb0
Robert Fry <robert.e.fry@protonmail.com>
2021-03-10 02:03:44 +0000

    Add a polynomial class template

---
+++ include/mathpp/poly.hh
@@ -0,0 +1,507 @@
+
+#ifndef __HH_MPP_POLY
+#define __HH_MPP_POLY
+
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <compare>
+
+/* ************************************************************************** */
+// Definitions
+/* ************************************************************************** */
+
+namespace mpp
+{
+
+    template <typename Tp>
+    class Poly
+    {
+    public:
+        Poly();
+        Poly(size_t, Tp const&);
+        Poly(std::vector<Tp> const&);
+        Poly(std::vector<Tp>&&);
+        Poly(std::initializer_list<Tp>);
+
+        template <typename Tq>
+        Poly(Poly<Tq> const&);
+
+        template <typename Tq>
+        Poly<Tp>& operator=(Poly<Tq> const&);
+        template <typename Tq>
+        Poly<Tp>& operator=(std::initializer_list<Tq>);
+
+    private:
+        void validate();
+
+    public:
+        auto coeffs() const -> std::vector<Tp> const& { return m_Coefficients; }
+        auto order() const { return m_Coefficients.size()-1; }
+        auto size() const { return m_Coefficients.size(); }
+
+        void assign(size_t, Tp const&);
+        void assign(std::vector<Tp> const&);
+        void assign(std::vector<Tp>&&);
+        void assign(std::initializer_list<Tp>);
+        void clear();
+
+        auto operator[](size_t i) const -> Tp const& { return m_Coefficients[i]; }
+        auto operator[](size_t i) -> Tp& { return m_Coefficients[i]; }
+        auto at(size_t i) const -> Tp const& { return m_Coefficients.at(i); }
+        auto at(size_t i) -> Tp& { return m_Coefficients.at(i); }
+
+        void swap(Poly<Tp>& other) { std::swap(m_Coefficients, other.m_Coefficients); }
+
+        Poly<Tp>& operator<<=(size_t);
+        Poly<Tp>& operator>>=(size_t);
+
+        template <typename Tq>
+        Poly<Tp>& operator+=(Poly<Tq> const&);
+        template <typename Tq>
+        Poly<Tp>& operator-=(Poly<Tq> const&);
+        template <typename Tq>
+        Poly<Tp>& operator*=(Poly<Tq> const&);
+        template <typename Tq>
+        Poly<Tp>& operator/=(Poly<Tq> const&);
+        template <typename Tq>
+        Poly<Tp>& operator%=(Poly<Tq> const&);
+
+    private:
+        std::vector<Tp> m_Coefficients{};
+    };
+
+    template <typename Tp, typename Tq>
+    auto divide_euclidean(mpp::Poly<Tp> const&, mpp::Poly<Tq> const&);
+
+} // namespace mpp
+
+/* ************************************************************************** */
+// Implementation
+/* ************************************************************************** */
+
+template <typename Tp>
+mpp::Poly<Tp>::Poly()
+    : m_Coefficients{Tp{}}
+{
+}
+
+template <typename Tp>
+mpp::Poly<Tp>::Poly(size_t n, Tp const& val)
+    : m_Coefficients(n,val)
+{
+    validate();
+}
+
+template <typename Tp>
+mpp::Poly<Tp>::Poly(std::vector<Tp> const& coeffs)
+    : m_Coefficients{coeffs}
+{
+    validate();
+}
+
+template <typename Tp>
+mpp::Poly<Tp>::Poly(std::vector<Tp>&& coeffs)
+    : m_Coefficients{std::move(coeffs)}
+{
+    validate();
+}
+
+template <typename Tp>
+mpp::Poly<Tp>::Poly(std::initializer_list<Tp> coeffs)
+    : m_Coefficients{coeffs}
+{
+    validate();
+}
+
+template <typename Tp>
+template <typename Tq>
+mpp::Poly<Tp>::Poly(Poly<Tq> const& other)
+{
+    m_Coefficients.reserve(other.size());
+
+    for (auto const& elem : other.coeffs())
+    {
+        m_Coefficients.push_back(static_cast<Tp>(elem));
+    }
+    validate();
+}
+
+template <typename Tp>
+template <typename Tq>
+mpp::Poly<Tp>& mpp::Poly<Tp>::operator=(Poly<Tq> const& other)
+{
+    m_Coefficients.clear();
+    m_Coefficients.reserve(other.size());
+
+    for (auto const& elem : other.coeffs())
+    {
+        m_Coefficients.push_back(static_cast<Tp>(elem));
+    }
+    validate();
+    return *this;
+}
+
+
+template <typename Tp>
+template <typename Tq>
+mpp::Poly<Tp>& mpp::Poly<Tp>::operator=(std::initializer_list<Tq> coeffs)
+{
+    m_Coefficients = coeffs;
+    validate();
+    return *this;
+}
+
+template <typename Tp>
+void mpp::Poly<Tp>::validate()
+{
+    auto itr = std::find_if(m_Coefficients.rbegin(), m_Coefficients.rend(), [](auto coeff){ return coeff != Tp{}; });
+    m_Coefficients.erase(itr.base(), m_Coefficients.end());
+    if (m_Coefficients.size() == 0) m_Coefficients.emplace_back();
+}
+
+template <typename Tp>
+void mpp::Poly<Tp>::assign(size_t n, Tp const& val)
+{
+    m_Coefficients = std::vector<Tp>(n,val);
+    validate();
+}
+
+template <typename Tp>
+void mpp::Poly<Tp>::assign(std::vector<Tp> const& coeffs)
+{
+    m_Coefficients = coeffs;
+    validate();
+}
+
+template <typename Tp>
+void mpp::Poly<Tp>::assign(std::vector<Tp>&& coeffs)
+{
+    m_Coefficients = std::move(coeffs);
+    validate();
+}
+
+template <typename Tp>
+void mpp::Poly<Tp>::assign(std::initializer_list<Tp> coeffs)
+{
+    m_Coefficients = coeffs;
+    validate();
+}
+
+template <typename Tp>
+void mpp::Poly<Tp>::clear()
+{
+    m_Coefficients.clear();
+    validate();
+}
+
+template <typename Tp>
+mpp::Poly<Tp>& mpp::Poly<Tp>::operator<<=(size_t n)
+{
+    if (m_Coefficients.size() <= n)
+    {
+        *this = Poly<Tp>{};
+    }
+    else
+    {
+        m_Coefficients.erase(m_Coefficients.begin(), m_Coefficients.begin()+n);
+    }
+    return *this;
+}
+
+template <typename Tp>
+mpp::Poly<Tp>& mpp::Poly<Tp>::operator>>=(size_t n)
+{
+    m_Coefficients.insert(m_Coefficients.begin(), n, Tp{});
+    return *this;
+}
+
+template <typename Tp>
+template <typename Tq>
+mpp::Poly<Tp>& mpp::Poly<Tp>::operator+=(Poly<Tq> const& other)
+{
+    m_Coefficients.resize(std::max(size(),other.size()));
+    for (size_t i = 0; i < other.size(); ++i) {
+        m_Coefficients[i] += other[i];  // could be zero
+    }
+    validate();
+    return *this;
+}
+
+template <typename Tp>
+template <typename Tq>
+mpp::Poly<Tp>& mpp::Poly<Tp>::operator-=(Poly<Tq> const& other)
+{
+    m_Coefficients.resize(std::max(size(),other.size()));
+    for (size_t i = 0; i < other.size(); ++i) {
+        m_Coefficients[i] -= other[i];  // could be zero
+    }
+    validate();
+    return *this;
+}
+
+template <typename Tp>
+template <typename Tq>
+mpp::Poly<Tp>& mpp::Poly<Tp>::operator*=(Poly<Tq> const& other)
+{
+    std::vector<Tp> coeffs;
+    coeffs.resize(size()*other.size());
+
+    for (size_t i = 0; i < size(); ++i)
+    {
+        for (size_t j = 0; j < other.size(); ++j)
+        {
+            coeffs[i+j] += m_Coefficients[i] * other[j];
+        }
+    }
+    assign(coeffs); // validates
+    return *this;
+}
+
+template <typename Tp>
+template <typename Tq>
+mpp::Poly<Tp>& mpp::Poly<Tp>::operator/=(Poly<Tq> const& other)
+{
+    mpp::Poly<Tp> quotient;
+
+    while (*this > other)
+    {
+        auto const coeff = coeffs().back() / other.coeffs().back();
+        auto const power = order() - other.order();
+        auto const term = mpp::Poly<Tp>{coeff} >>= power;
+        quotient += term;
+        *this -= term * other;
+    }
+    return *this = std::move(quotient);
+}
+
+template <typename Tp>
+template <typename Tq>
+mpp::Poly<Tp>& mpp::Poly<Tp>::operator%=(Poly<Tq> const& other)
+{
+    while (*this > other)
+    {
+        auto const coeff = coeffs().back() / other.coeffs().back();
+        auto const power = order() - other.order();
+        *this -= other * (mpp::Poly<Tp>{coeff} >>= power);
+    }
+    return *this;
+}
+
+/* ************************************************************************** */
+// Namespace Extensions
+/* ************************************************************************** */
+
+template <typename Tp, typename Tq>
+auto mpp::divide_euclidean(mpp::Poly<Tp> const& poly1, mpp::Poly<Tq> const& poly2)
+{
+    using Tr = decltype(poly1[0]/poly2[0]);
+    using Ts = decltype(poly1[0]%poly2[0]);
+    mpp::Poly<Tr> quotient;
+    mpp::Poly<Ts> remainder = poly1;
+
+    while (remainder > poly2)
+    {
+        auto const coeff = remainder.coeffs().back() / poly2.coeffs().back();
+        auto const power = remainder.order() - poly2.order();
+        auto const term = mpp::Poly<Tr>{coeff} >>= power;
+        quotient += term;
+        remainder -= term * poly2;
+    }
+    return std::tuple<mpp::Poly<Tr>,mpp::Poly<Ts>>{quotient,remainder};
+}
+
+/* ************************************************************************** */
+// External Extensions
+/* ************************************************************************** */
+
+template <typename Tp, typename Tq>
+bool operator==(mpp::Poly<Tp> const& poly1, mpp::Poly<Tq> const& poly2)
+{
+    if (poly1.order() != poly2.order()) return false;
+
+    for (size_t i = poly1.order(); i <= poly1.order(); --i)
+    {
+        if (poly1[i] != poly2[i]) return false;
+    }
+    return true;
+}
+
+template <typename Tp>
+bool operator==(mpp::Poly<Tp> const& poly1, mpp::Poly<Tp> const& poly2)
+{
+    return poly1.coeffs() == poly2.coeffs();
+}
+
+template <typename Tp, typename Tq>
+std::strong_ordering operator<=>(mpp::Poly<Tp> const& poly1, mpp::Poly<Tq> const& poly2)
+{
+    if (poly1.order() > poly2.order()) return std::strong_ordering::greater;
+    if (poly1.order() < poly2.order()) return std::strong_ordering::less;
+
+    const size_t order = poly1.order();
+    for (size_t i = order; i <= order; --i)
+    {
+        if (poly1[i] > poly2[i]) return std::strong_ordering::greater;
+        if (poly1[i] < poly2[i]) return std::strong_ordering::less;
+    }
+    return std::strong_ordering::equivalent;
+}
+
+template <typename Tp>
+std::strong_ordering operator<=>(mpp::Poly<Tp> const& poly1, mpp::Poly<Tp> const& poly2)
+{
+    if (poly1.order() > poly2.order()) return std::strong_ordering::greater;
+    if (poly1.order() < poly2.order()) return std::strong_ordering::less;
+
+    const size_t order = poly1.order();
+    for (size_t i = order; i <= order; --i)
+    {
+        if (poly1[i] > poly2[i]) return std::strong_ordering::greater;
+        if (poly1[i] < poly2[i]) return std::strong_ordering::less;
+    }
+    return std::strong_ordering::equal;
+}
+
+template <typename Tp>
+auto operator<<(mpp::Poly<Tp> const& poly, size_t n)
+{
+    if (poly.size() <= n) {
+        return mpp::Poly<Tp>{};
+    }
+
+    auto coeffs = std::vector<Tp>{poly.coeffs().begin()+n,poly.coeffs().end()};
+    return mpp::Poly<Tp>{coeffs};
+}
+
+template <typename Tp>
+auto operator>>(mpp::Poly<Tp> const& poly, size_t n)
+{
+    auto coeffs = std::vector<Tp>(n,Tp{});
+    coeffs.insert(coeffs.end(),poly.coeffs().begin(),poly.coeffs().end());
+    return mpp::Poly<Tp>{coeffs};
+}
+
+template <typename Tp>
+auto operator+(mpp::Poly<Tp> const& poly)
+{
+    using Tr = decltype(Tp{}+poly[0]);
+    std::vector<Tr> coeffs;
+    coeffs.reserve(poly.size());
+
+    for (auto const& coeff : poly.coeffs())
+    {
+        coeffs.push_back(Tp{}+coeff);
+    }
+    return mpp::Poly<Tr>{coeffs};
+}
+
+template <typename Tp>
+auto operator-(mpp::Poly<Tp> const& poly)
+{
+    using Tr = decltype(Tp{}-poly[0]);
+    std::vector<Tr> coeffs;
+    coeffs.reserve(poly.size());
+
+    for (auto const& coeff : poly.coeffs())
+    {
+        coeffs.push_back(Tp{}-coeff);
+    }
+    return mpp::Poly<Tr>{coeffs};
+}
+
+template <typename Tp, typename Tq>
+auto operator+(mpp::Poly<Tp> const& poly1, mpp::Poly<Tq> const& poly2)
+{
+    using Tr = decltype(poly1[0]+poly2[0]);
+    std::vector<Tr> coeffs;
+    coeffs.reserve(std::max(poly1.size(),poly2.size()));
+
+    size_t index = 0;
+    for (; index <= std::min(poly1.order(),poly2.order()); ++index)
+    {
+        coeffs.push_back(poly1[index]+poly2[index]);
+    }
+    for (; index <= poly1.order(); ++index)
+    {
+        coeffs.push_back(poly1[index]+Tq{});
+    }
+    for (; index <= poly2.order(); ++index)
+    {
+        coeffs.push_back(Tp{}+poly2[index]);
+    }
+    return mpp::Poly<Tr>{std::move(coeffs)};
+}
+
+template <typename Tp, typename Tq>
+auto operator-(mpp::Poly<Tp> const& poly1, mpp::Poly<Tq> const& poly2)
+{
+    using Tr = decltype(poly1[0]-poly2[0]);
+    std::vector<Tr> coeffs;
+    coeffs.reserve(std::max(poly1.size(),poly2.size()));
+
+    size_t index = 0;
+    for (; index <= std::min(poly1.order(),poly2.order()); ++index)
+    {
+        coeffs.push_back(poly1[index]-poly2[index]);
+    }
+    for (; index <= poly1.order(); ++index)
+    {
+        coeffs.push_back(poly1[index]-Tq{});
+    }
+    for (; index <= poly2.order(); ++index)
+    {
+        coeffs.push_back(Tp{}-poly2[index]);
+    }
+    return mpp::Poly<Tr>{std::move(coeffs)};
+}
+
+template <typename Tp, typename Tq>
+auto operator*(mpp::Poly<Tp> const& poly1, mpp::Poly<Tq> const& poly2)
+{
+    using Tr = decltype(poly1[0]*poly2[0]);
+    std::vector<Tr> coeffs;
+    coeffs.resize(poly1.size()*poly2.size());
+
+    for (size_t i = 0; i <= poly1.order(); ++i)
+    {
+        for (size_t j = 0; j <= poly2.order(); ++j)
+        {
+            coeffs[i+j] += poly1[i] * poly2[j];
+        }
+    }
+    auto itr = std::find_if(coeffs.rbegin(),coeffs.rend(),[](auto coeff){ return coeff != Tr{}; });
+    coeffs.erase(itr.base(),coeffs.end());
+    return mpp::Poly<Tr>{std::move(coeffs)};
+}
+
+template <typename Tp, typename Tq>
+auto operator/(mpp::Poly<Tp> const& poly1, mpp::Poly<Tq> const& poly2)
+{
+    auto result = mpp::divide_euclidean(poly1,poly2);
+    return std::get<0>(result);
+}
+
+template <typename Tp, typename Tq>
+auto operator%(mpp::Poly<Tp> const& poly1, mpp::Poly<Tq> const& poly2)
+{
+    auto result = mpp::divide_euclidean(poly1,poly2);
+    return std::get<1>(result);
+}
+
+/* ************************************************************************** */
+// Standard Extensions
+/* ************************************************************************** */
+
+namespace std
+{
+
+    template <typename Tp>
+    void swap(mpp::Poly<Tp>& poly1, mpp::Poly<Tp>& poly2)
+    {
+        poly1.swap(poly2);
+    }
+
+} // namespace std
+
+#endif /* __HH_MPP_POLY */
---
+++ test/test_poly.cc
@@ -0,0 +1,217 @@
+
+#include "gtest/gtest.h"
+#include <mathpp/poly.hh>
+
+TEST(MPP_POLY, LIFETIME)
+{
+    {
+        auto vec = std::vector<int>{0};
+        auto poly = mpp::Poly<int>{};                       // default
+        EXPECT_EQ(poly.coeffs(), vec);
+    }
+    {
+        auto vec = std::vector<int>{1,1,1};
+        auto poly = mpp::Poly<int>(3,1);                    // fill
+        EXPECT_EQ(poly.coeffs(), vec);
+    }
+    {
+        auto vec = std::vector<int>{1,2,3,4,5};
+        auto poly = mpp::Poly<int>{1,2,3,4,5};              // forward coeffs
+        EXPECT_EQ(poly.coeffs(), vec);
+    }
+    {
+        auto vec = std::vector<int>{1,2,3,4,5};
+        auto poly = mpp::Poly<int>{{1,2,3,4,5}};            // move coeffs
+        EXPECT_EQ(poly.coeffs(), vec);
+    }
+    {
+        auto vec = std::vector<int>{1,2,3,4,5};
+        auto poly = mpp::Poly<int>{vec};                    // copy coeffs
+        EXPECT_EQ(poly.coeffs(), vec);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{1,2,3,4,5};
+        auto poly2 = mpp::Poly<int>{poly1};                 // copy construct
+        EXPECT_EQ(poly1.coeffs(), poly2.coeffs());
+    }
+    {
+        auto poly1 = mpp::Poly<int>{1,2,3,4,5};
+        auto poly2 = mpp::Poly<int>{};
+        poly2 = poly1;                                      // copy assign
+        EXPECT_EQ(poly1.coeffs(), poly2.coeffs());
+    }
+    {
+        auto vec = std::vector<int>{1,2,3,4,5};
+        auto poly1 = mpp::Poly<int>{vec};
+        auto poly2 = mpp::Poly<int>{std::move(poly1)};      // move construct
+        EXPECT_EQ(poly2.coeffs(), vec);
+    }
+    {
+        auto vec = std::vector<int>{1,2,3,4,5};
+        auto poly1 = mpp::Poly<int>{vec};
+        auto poly2 = mpp::Poly<int>{};
+        poly2 = std::move(poly1);                           // move assign
+        EXPECT_EQ(poly2.coeffs(), vec);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{1,2,3,4,5};
+        auto poly2 = mpp::Poly<float>{poly1};               // copy cast construct
+        EXPECT_TRUE(poly1 == poly2);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{1,2,3,4,5};
+        auto poly2 = mpp::Poly<float>{};
+        poly2 = poly1;                                      // copy cast assign
+        EXPECT_TRUE(poly1 == poly2);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{1,2,3,4,5};
+        auto poly2 = mpp::Poly<int>{poly1};
+        auto poly3 = mpp::Poly<float>{std::move(poly2)};    // move cast construct
+        EXPECT_TRUE(poly1 == poly3);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{1,2,3,4,5};
+        auto poly2 = mpp::Poly<int>{poly1};
+        auto poly3 = mpp::Poly<float>{};
+        poly3 = std::move(poly2);                           // move cast assign
+        EXPECT_TRUE(poly1 == poly3);
+    }
+}
+
+TEST(MPP_POLY, OPERATORS)
+{
+    {
+        auto poly1 = mpp::Poly<int>{1,2,3};
+        auto poly2 = mpp::Poly<float>{1.0f,2.0f,3.0f};
+        EXPECT_TRUE(poly1 == poly2);
+        EXPECT_TRUE(poly1 >= poly2);
+        EXPECT_TRUE(poly1 <= poly2);
+        EXPECT_FALSE(poly1 != poly2);
+        EXPECT_FALSE(poly1 < poly2);
+        EXPECT_FALSE(poly1 > poly2);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{1,2,3};
+        auto poly2 = mpp::Poly<float>{1.0f,1.0f,1.0f};
+        EXPECT_TRUE(poly1 != poly2);
+        EXPECT_TRUE(poly1 >= poly2);
+        EXPECT_TRUE(poly1 > poly2);
+        EXPECT_FALSE(poly1 == poly2);
+        EXPECT_FALSE(poly1 <= poly2);
+        EXPECT_FALSE(poly1 < poly2);
+    }
+    {
+        auto poly1 = mpp::Poly<float>{1.0f,2.0f,3.5f};
+        auto poly2 = mpp::Poly<float>{1.0f,2.5f,3.0f};
+        EXPECT_TRUE(poly1 != poly2);
+        EXPECT_TRUE(poly1 >= poly2);
+        EXPECT_TRUE(poly1 > poly2);
+        EXPECT_FALSE(poly1 == poly2);
+        EXPECT_FALSE(poly1 <= poly2);
+        EXPECT_FALSE(poly1 < poly2);
+    }
+    {
+        auto vec1 = std::vector<int>{1,2,3}, vec2 = std::vector<int>{4,5,6};
+        auto poly1 = mpp::Poly<int>{vec1}, poly2 = mpp::Poly<int>{vec2};
+
+        std::swap(poly1,poly2);
+        EXPECT_EQ(poly1.coeffs(), vec2);
+        EXPECT_EQ(poly2.coeffs(), vec1);
+
+        poly1.swap(poly2);
+        EXPECT_EQ(poly1.coeffs(), vec1);
+        EXPECT_EQ(poly2.coeffs(), vec2);
+    }
+}
+
+TEST(MPP_POLY, MATH_SELF)
+{
+    {
+        auto poly1 = mpp::Poly<int>{1,2,3,4,5};
+
+        auto poly2 = mpp::Poly<int>{3,4,5};
+        EXPECT_TRUE((poly1 << 2) == poly2);
+        EXPECT_TRUE((poly1 <<= 2) == poly2);
+
+        auto poly3 = mpp::Poly<int>{0,0,3,4,5};
+        EXPECT_TRUE((poly1 >> 2) == poly3);
+        EXPECT_TRUE((poly1 >>= 2) == poly3);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{5,1};           // 5+x
+        poly1 += mpp::Poly<float>{0.0f,2.0f,1.0f};  // 2x+x^2
+        auto poly2 = mpp::Poly<int>{5,3,1};         // 5+3x+x^2
+        EXPECT_TRUE(poly1 == poly2);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{5,1};           // 5+x
+        poly1 -= mpp::Poly<float>{0.0f,2.0f,1.0f};  // 2x+x^2
+        auto poly2 = mpp::Poly<int>{5,-1,-1};       // 5-x-x^2
+        EXPECT_TRUE(poly1 == poly2);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{5,1};           // 5+x
+        poly1 *= mpp::Poly<float>{0.0f,2.0f,1.0f};  // 2x+x^2
+        auto poly2 = mpp::Poly<int>{0,10,7,1};      // 10x+7x^2+x^3
+        EXPECT_TRUE(poly1 == poly2);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{2,5,7,6};
+        auto poly2 = mpp::Poly<int>{1,2,3};
+        auto poly3 = mpp::Poly<int>{1,2};
+        EXPECT_TRUE((poly1 /= poly2) == poly3);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{2,5,7,6};
+        auto poly2 = mpp::Poly<int>{1,2,3};
+        auto poly3 = mpp::Poly<int>{1,1};
+        EXPECT_TRUE((poly1 %= poly2) == poly3);
+    }
+}
+
+TEST(MPP_POLY, MATH_EXTRA)
+{
+    {
+        auto poly1 = mpp::Poly<int>{1,2,3};
+
+        auto poly2 = mpp::Poly<int>{-1,-2,-3};
+        EXPECT_TRUE(-poly1 == poly2);
+
+        auto poly3 = mpp::Poly<int>{+1,+2,+3};
+        EXPECT_TRUE(+poly1 == poly3);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{5,1};                       // 5+x
+        auto poly2 = mpp::Poly<float>{0.0f,2.0f,1.0f};          // 2x+x^2
+        auto poly3 = mpp::Poly<float>{5.0f,3.0f,1.0f};          // 5+3x+x^2
+        auto poly4 = poly1 + poly2;
+        EXPECT_TRUE(poly3 == poly4);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{5,1};                       // 5+x
+        auto poly2 = mpp::Poly<float>{0.0f,2.0f,1.0f};          // 2x+x^2
+        auto poly3 = mpp::Poly<float>{5.0f,-1.0f,-1.0f};        // 5-x-x^2
+        auto poly4 = poly1 - poly2;
+        EXPECT_TRUE(poly3 == poly4);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{5,1};                       // 5+x
+        auto poly2 = mpp::Poly<float>{0.0f,2.0f,1.0f};          // 2x+x^2
+        auto poly3 = mpp::Poly<float>{0.0f,10.0f,7.0f,1.0f};    // 10x+7x^2+x^3
+        auto poly4 = poly1 * poly2;
+        EXPECT_TRUE(poly3 == poly4);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{2,5,7,6};
+        auto poly2 = mpp::Poly<int>{1,2,3};
+        auto poly3 = mpp::Poly<int>{1,2};
+        EXPECT_TRUE((poly1 / poly2) == poly3);
+    }
+    {
+        auto poly1 = mpp::Poly<int>{2,5,7,6};
+        auto poly2 = mpp::Poly<int>{1,2,3};
+        auto poly3 = mpp::Poly<int>{1,1};
+        EXPECT_TRUE((poly1 % poly2) == poly3);
+    }
+}
